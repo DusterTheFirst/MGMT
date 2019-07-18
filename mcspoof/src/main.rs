02fb1fc2439611e43d3f73fbe3a33a0ca9209bfe
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2019-07-18 17:50:11 -0400

    Pingpong

--- mcspoof/src/main.rs
+++ mcspoof/src/main.rs
@@ -1,14 +1,10 @@
 use std::net::{TcpListener, SocketAddr, IpAddr, Ipv4Addr};
-use std::io::prelude::*;
 use std::thread;
-use byteorder::{BigEndian, ByteOrder};
-use serde::Serialize;
-use serde_json::ser;
 use ansi_term::Color::*;
 
 mod proto;
 
-use proto::{ReadPacket, WritePacket, Handshake};
+use proto::{PacketManipulation, Handshake, response::{Description, Player, Players, Response, Version}};
 
 // TODO: CLEAN UP
 // TODO: DETECT PLAYER JOIN
@@ -24,45 +20,23 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
+            Ok(mut stream) => {
                 thread::Builder::new().name("Stream Handler".to_owned()).spawn(move || {
                     println!("{} {}", Green.paint("New connection:"), stream.peer_addr().unwrap());
-
-                    let mut reader = std::io::BufReader::new(&stream);
-                    let mut writer = std::io::BufWriter::new(&stream);
-
-                    // connection succeeded
-                
-                    println!("{}", RGB(128, 128, 128).paint("START HANDSHAKE"));
-                    
+                                    
                     // First, the client sends a Handshake packet with its state set to 1.                     
-                    let handshake: Handshake = reader.read_handshake().unwrap();
-                    if handshake.packet.length == 254 {
-                        println!("{}", Red.paint("LEGACY HANDSHAKE, RETURNING\n\n"));
-                        return;
-                    }
-                    println!("Length: {:?} bytes", handshake.packet.length);
-                    println!("Packet ID: 0x{:02X}", handshake.packet.id);
-                    println!("-- DATA --");
-                    println!("Protocol version: {}", handshake.protocol_version);
-                    println!("Address: {}", handshake.address);
-                    println!("Port: {}", handshake.port);
-                    println!("Next State (1 = ping, 2 = connect): {:?}", handshake.next_state);
-
-                    println!("{}", RGB(128, 128, 128).paint("END HANDSHAKE\n\n"));
-
-                    println!("{}", RGB(128, 128, 128).paint("START REQUEST"));
+                    let handshake: Handshake = stream.read_handshake().unwrap();
+                    println!("{}", RGB(128, 128, 128).paint("HANDSHAKE"));
+                    println!("{}\n\n", handshake);
 
                     // The client follows up with a Request packet. This packet has no fields. 
-                    let packet = reader.read_packet().unwrap();
-                    println!("Length: {:?} bytes", packet.length);
-                    println!("Packet ID: 0x{:02X}", packet.id);
-
-                    println!("{}", RGB(128, 128, 128).paint("END REQUEST\n\n"));
+                    let packet = stream.read_packet().unwrap();
+                    println!("{}", RGB(128, 128, 128).paint("REQUEST"));
+                    println!("{}", packet);
                     
                     // The server should respond with a Response packet.
-                    println!("{}", RGB(128, 128, 128).paint("START RESPONSE"));
-                    let response = ser::to_string(&Response {
+                    println!("{}", RGB(128, 128, 128).paint("RESPONSE"));
+                    let response = Response {
                         version: Version {
                             name: String::from("Server Alseep"),
                             protocol: 0
@@ -108,38 +82,16 @@ fn main() {
                             ])
                         },
                         favicon: Some("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4wcREyI4l1HM8QAAAPFJREFUeNrt2sENwjAQRNEY0QoNUAINpFYaoAQa4Egh5go+xFoZiC2/uQUpUvZr/yRIXhYRmTkpesMtpfx+fcp51wEe6XOES86hmQ6zbwAAOqCS57LkkQa6F9drZUYKAKAD2t7zre/haKKdVOsECgAweY7lD6M5Xz5P9L8JBQDQAbH05nz5PNHvBAoAoAP2TavzNgAAAMbqgL2dtwEAAPDfDujNeRsAAAC/7YDenbcBAADw3Q4YzXkbAAAAbR3Qm/OtZ5goAIAO2HZ49DNCNgAAADZT9fVaOHfufCBnhSkAwHc7oNYJvWUNzkQBAEREJs4Lb5hUptWWBL4AAAAASUVORK5CYII=".to_owned())
-                    }).unwrap();
-                    let response_prefixed = proto::string::write(&response);
-                    let written_packet = writer.write_packet(0x00, response_prefixed.as_slice()).unwrap();
-                    println!("Size: {} bytes", written_packet.length);
-                    println!("Packet ID: 0x{:02X}", written_packet.id);
-                    println!("Response size: {} bytes", response.len());
-                    println!("Response body: {}", response);
-                    writer.flush().unwrap();
+                    };
+                    let written_packet = stream.write_response(response).unwrap();
+                    println!("{}", written_packet);
 
-                    println!("{}", RGB(128, 128, 128).paint("END RESPONSE\n\n"));
-
-                    println!("{}", RGB(128, 128, 128).paint("START PING"));
-                    
                     // If the process is continued, the client will now send a Ping packet containing some payload which is not important.
-                    let packet = reader.read_packet().unwrap(); 
-                    println!("Length: {:?} bytes", packet.length);
-                    println!("Packet ID: 0x{:02X}", packet.id);
-                    let payload = BigEndian::read_i64(packet.data.as_slice());
-                    println!("Payload: 0x{:08X}", payload);
-
-                    println!("{}", RGB(128, 128, 128).paint("END PING\n\n"));
-
-                    println!("{}", RGB(128, 128, 128).paint("START PONG"));
-                    
                     // The server will respond with the Pong packet and then close the connection. 
-                    let written_packet = writer.write_packet(1, packet.data.as_slice()).unwrap();
-                    println!("Length: {:?} bytes", written_packet.length);
-                    println!("Packet ID: 0x{:02X}", written_packet.id);
-                    println!("Payload: 0x{:08X}", payload);
-
-                    println!("{}", RGB(128, 128, 128).paint("END PONG\n\n"));
-
+                    let packet = stream.ping_pong().unwrap(); 
+                    println!("{}", RGB(128, 128, 128).paint("PING PONG"));
+                    println!("{}", packet);
+                
                     // let mut buf = Vec::new();
                     // stream.read_to_end(&mut buf).unwrap();
                     // println!("{:?}", buf);
@@ -148,46 +100,4 @@ fn main() {
             Err(e) => eprintln!("{:?}", e)
         }
     }
-}
-
-#[derive(Serialize)]
-struct Response {
-    version: Version,
-    players: Players,	
-    description: Description,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    favicon: Option<String>
-}
-
-#[derive(Serialize)]
-struct Version {
-    name: String,
-    protocol: u16
-}
-
-#[derive(Serialize)]
-struct Players {
-    max: u16,
-    online: u16,
-    sample: Vec<Player>
-}
-
-#[derive(Serialize)]
-struct Player {
-    name: String,
-    id: String
-}
-
-#[derive(Serialize)]
-struct Description {
-    text: String,
-    bold: bool,
-    italic: bool,
-    underlined: bool,
-    strikethrough: bool,
-    obfuscated: bool,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    color: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    extra: Option<Vec<Description>>
 }
---
+++ mcspoof/src/proto/packet.rs
@@ -0,0 +1,109 @@
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Debug)]
+pub struct Packet {
+    pub length: i32,
+    pub id: i32,
+    pub data: Vec<u8>
+}
+
+impl Display for Packet {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        writeln!(f, "Packet Info:")?;
+        writeln!(f, "    Length: {:?} bytes", self.length)?;
+        writeln!(f, "    ID: 0x{:02X}", self.id)?;
+        writeln!(f, "    Data {:X?}", self.data)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct Handshake {
+    pub packet: Packet,
+    pub protocol_version: i32,
+    pub address: String,
+    pub port: u16,
+    pub next_state: NextState
+}
+
+impl Display for Handshake {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        writeln!(f, "Packet Info:")?;
+        writeln!(f, "    Length: {:?} bytes", self.packet.length)?;
+        writeln!(f, "    ID: 0x{:02X}", self.packet.id)?;
+        writeln!(f, "Handshake Data:")?;
+        writeln!(f, "    Protocol version: {}", self.protocol_version)?;
+        writeln!(f, "    Address: {}", self.address)?;
+        writeln!(f, "    Port: {}", self.port)?;
+        writeln!(f, "    Next State: {:?}", self.next_state)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum NextState {
+    Ping,
+    Connect,
+    Unknown(i32)
+}
+
+impl From<i32> for NextState {
+    fn from(num: i32) -> NextState {
+        match num {
+            1 => NextState::Ping,
+            2 => NextState::Connect,
+            _ => NextState::Unknown(num)
+        }
+    }
+}
+
+/// Response packet structs
+pub mod response {
+    use serde::Serialize;
+
+    /// The JSON response to a ping
+    #[derive(Serialize)]
+    pub struct Response {
+        pub version: Version,
+        pub players: Players,	
+        pub description: Description,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub favicon: Option<String>
+    }
+
+    /// The version part of the JSON response to a ping
+    #[derive(Serialize)]
+    pub struct Version {
+        pub name: String,
+        pub protocol: u16
+    }
+
+    #[derive(Serialize)]
+    pub struct Players {
+        pub max: u16,
+        pub online: u16,
+        pub sample: Vec<Player>
+    }
+
+    #[derive(Serialize)]
+    pub struct Player {
+        pub name: String,
+        pub id: String
+    }
+
+    #[derive(Serialize)]
+    pub struct Description {
+        pub text: String,
+        pub bold: bool,
+        pub italic: bool,
+        pub underlined: bool,
+        pub strikethrough: bool,
+        pub obfuscated: bool,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub color: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub extra: Option<Vec<Description>>
+    }
+}
